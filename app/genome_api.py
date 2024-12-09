8da19cec378b3fa2fae2c04f140821e39c8fa924
JonathanArroyaveGonzalez <arroyave3000g@gmail.com.com>
2024-12-09 02:18:24 -0500

    feat: Centralize API logic and include route

--- app/genome_api.py
+++ app/genome_api.py
@@ -1,6 +1,6 @@
 import time
 import os
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 from dotenv import load_dotenv
@@ -11,6 +11,8 @@ import asyncio
 
 # Configuración básica
 load_dotenv()
+
+router = APIRouter(prefix="/genome",tags=["Genome Parellel Processing"])
 
 logging.basicConfig(
     level=logging.INFO,
@@ -119,7 +121,7 @@ async def get_genome_data(
             detail=f"Error processing request: {str(e)}"
         )
 
-@app.on_event("startup")
+@router.on_event("startup")
 async def startup_event():
     try:
         global thread_pool
@@ -137,7 +139,7 @@ async def startup_event():
         logging.error(f"Error durante la inicialización: {str(e)}")
         raise
 
-@app.get("/genome/all")
+@router.get("/all")
 async def root(
     filter: str = "CHROM",
     search: str = "",
@@ -146,7 +148,7 @@ async def root(
 ):
     return await get_genome_data(filter, search, page, page_size)
 
-@app.get("/genome/search")
+@router.get("/search")
 async def search_variants(
     filter: str = "CHROM",
     search: str = "",
@@ -155,7 +157,7 @@ async def search_variants(
 ):
     return await get_genome_data(filter, search, page, page_size)
 
-@app.on_event("shutdown")
+@router.on_event("shutdown")
 async def shutdown_event():
     if thread_pool:
         thread_pool.shutdown(wait=True)
